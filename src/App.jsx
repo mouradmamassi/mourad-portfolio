626fda318b70f61f645cf3123f1d37be3554d117
Mourad MAMASSI <mourad.mamassi@gmail.com>
2025-08-15 18:34:11 +0200

    🔥 deleting contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import HeroSection from "./components/HeroSection";
 import PortfolioSection from "./components/PortfolioSection";
-import ContactForm from "./components/ContactForm";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
     <div className="font-sans">
       <HeroSection />
       <PortfolioSection />
-      <ContactForm />
       <Footer />
     </div>
   );
